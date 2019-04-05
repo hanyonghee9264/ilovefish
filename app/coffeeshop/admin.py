bfa4eec4ab81e3ba972ead5f97c3455659649bed
hanyonghee <hanyonghee9264@gmail.com>
2019-04-05 20:25:29 +0900

    [UPDATE] Django Admin Custom coffee

--- app/coffeeshop/admin.py
+++ app/coffeeshop/admin.py
@@ -3,7 +3,17 @@ from django.contrib import admin
 from .models import CoffeeCategory, Coffee, CoffeeImage
 
 admin.site.register(CoffeeCategory)
-admin.site.register(Coffee)
+# admin.site.register(Coffee)
 admin.site.register(CoffeeImage)
 
 
+# 스타벅스 커피 list 형식
+@admin.register(Coffee)
+class CoffeeAdmin(admin.ModelAdmin):
+    list_per_page = 25
+    list_display = (
+        'name', 'coffee_info', 'coffee_size', 'calorie',
+        'saturated_fat', 'protein', 'sodium', 'sugars',
+        'caffeine',
+    )
+    search_fields = ('name',)
