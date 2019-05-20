ec71d606636032d52f4da14466849cfd6b33b422
hanyonghee <hanyonghee9264@gmail.com>
2019-05-20 12:52:45 +0900

    [ADD] task 의 name 설정

--- app/coffeeshop/tasks.py
+++ app/coffeeshop/tasks.py
@@ -2,7 +2,7 @@ from coffeeshop.crawling.starbucks import Starbucks
 from config import celery_app
 
 
-@celery_app.task
+@celery_app.task(name='starbucks_crawling')
 def starbucks_crawling():
     Starbucks.get_coffee_info()
     print('종료')
