b476bdef04febe349c6e70cf072df6f44cd3e0b3
hanyonghee <hanyonghee9264@gmail.com>
2019-05-25 22:21:54 +0900

    [ADD] datetime 추가 및 Email 기능

--- app/coffeeshop/tasks.py
+++ app/coffeeshop/tasks.py
@@ -1,3 +1,7 @@
+import datetime
+
+from django.core.mail import EmailMessage
+
 from coffeeshop.crawling.starbucks import Starbucks
 from config import celery_app
 
@@ -5,4 +9,13 @@ from config import celery_app
 @celery_app.task(name='starbucks_crawling')
 def starbucks_crawling():
     Starbucks.get_coffee_info()
+
+    # 위의 크롤링 함수가 종료되면 내 이메일 계정에 보고
+    now_date = datetime.datetime.now().strftime('%Y-%m-%d')
+
+    crawling_result = f'{now_date} 에 대한 크롤링 완료 메세지'
+    to_email = 'hanyonghee9264@gmail.com'
+
+    email = EmailMessage(crawling_result, )
+    # EmailMessage
     print('종료')
