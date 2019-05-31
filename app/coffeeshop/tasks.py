8a6e4786baaf15ecf2cab2779f5ae89f41c5f9f2
hanyonghee <hanyonghee9264@gmail.com>
2019-05-31 19:19:23 +0900

    [UPDATE] 크롤링에 대한 결과 이메일 코드 추가 및 ecs 사용으로인한 dockerrun 제거

--- app/coffeeshop/tasks.py
+++ app/coffeeshop/tasks.py
@@ -1,8 +1,9 @@
 import datetime
 
 from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 
-from coffeeshop.crawling.starbucks import Starbucks
+from coffeeshop.crawling.starbucks import *
 from config import celery_app
 
 
@@ -10,12 +11,16 @@ from config import celery_app
 def starbucks_crawling():
     Starbucks.get_coffee_info()
 
+    crawling_log = dict_log
+
+    message = render_to_string('coffeeshop/crawling_result_email.html',
+                               {'dict_log': crawling_log},
+    )
     # 위의 크롤링 함수가 종료되면 내 이메일 계정에 보고
     now_date = datetime.datetime.now().strftime('%Y-%m-%d')
 
     crawling_result = f'{now_date} 에 대한 크롤링 완료 메세지'
     to_email = 'hanyonghee9264@gmail.com'
 
-    email = EmailMessage(crawling_result, )
-    # EmailMessage
-    print('종료')
+    email = EmailMessage(crawling_result, message, to=[to_email])
+    email.send()
