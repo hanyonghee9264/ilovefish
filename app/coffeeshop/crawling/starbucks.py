bea5de7c46e556795a056231843fe4de11b5ae19
hanyonghee <hanyonghee9264@gmail.com>
2019-06-05 21:00:18 +0900

    [ADD] 크롤링 코드 변경

--- app/coffeeshop/crawling/starbucks.py
+++ app/coffeeshop/crawling/starbucks.py
@@ -82,7 +82,7 @@ class Starbucks:
                 driver.implicitly_wait(15)
                 # selenium 팝업창 해결
                 try:
-                    WebDriverWait(driver, 5).until(EC.alert_is_present(),
+                    WebDriverWait(driver, 3).until(EC.alert_is_present(),
                                                      'Timed out waiting for PA creation ' +
                                                      'confirmation popup to appear.')
 
@@ -157,5 +157,6 @@ class Starbucks:
                             except FileExistsError:
                                 print('이미 존재하는 파일')
                         time.sleep(0.8)
-            return dict_log
         driver.close()
+
+        return dict_log
