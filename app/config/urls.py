ba54ab042fa6c72ea8f415ec12ca73c8a48077e7
hanyonghee <hanyonghee9264@gmail.com>
2019-06-07 16:31:23 +0900

    [UPDATE] config view 추가 및 media 경로 설정

--- app/config/urls.py
+++ app/config/urls.py
@@ -13,12 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from coffeeshop import views
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.main_page, name='main-page')
+    path('', views.main_page, name='main-page'),
+    path('coffeeshop/', include('coffeeshop.urls')),
 ]
+
+urlpatterns += static(
+    prefix=settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
