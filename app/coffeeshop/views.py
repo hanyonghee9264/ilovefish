2897520f152bcdb42d5d04c6432b384829131173
hanyonghee <hanyonghee9264@gmail.com>
2019-06-12 23:00:24 +0900

    [ADD] 스타벅스 카테고리 별 커피 views 정리 TEST#1

--- app/coffeeshop/views.py
+++ app/coffeeshop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import CoffeeImage, Coffee
+from .models import Coffee, CoffeeCategory
 
 
 def starbucks_list(request):
@@ -8,4 +8,14 @@ def starbucks_list(request):
     context = {
         'details': starbucks_details,
     }
-    return render(request, 'coffeeshop/starbucks_list.html', context)
+    return render(request, 'coffeeshop/starbucks/starbucks_list.html', context)
+
+
+def starbucks_filtering(request):
+    coffee = Coffee.objects.all()
+    # coldbrew = Coffee.objects.filter(category__name__contains='콜드 브루 커피')
+    context = {
+        'coffee': coffee,
+        'coldbrew': coldbrew,
+    }
+    return render(request, 'coffeeshop/starbucks/starbucks_coldbrew.html', context)
