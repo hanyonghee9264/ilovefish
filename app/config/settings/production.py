72e590a822126db60375289384c80d221207e113
hanyonghee <hanyonghee9264@gmail.com>
2019-06-27 16:54:20 +0900

    [UPDATE] Elasticache 엔드포인트 설정

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -17,12 +17,17 @@ ALLOWED_HOSTS = [
 
 DATABASES = secrets['DATABASES']
 
-# Celery
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+# Celery [로컬]
+# CELERY_BROKER_URL = 'redis://localhost:6379/0'
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+
+# Celery + redis
+CELERY_BROKER_URL = 'redis://' + AWS_ELASTIC_CACHE
+CELERY_RESULT_BACKEND = 'redis://' + AWS_ELASTIC_CACHE
 
 CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TAST_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'Asia/Seoul' #Celery beat가 스케줄러이기 때문에 시간에 대한 정의를 해야함
 
