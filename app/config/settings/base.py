82833b13e216c469d3f357b23540269b3e319e96
hanyonghee <hanyonghee9264@gmail.com>
2019-07-12 15:10:38 +0900

    [DELETE] 불필요한 내용 삭제

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 ROOT_DIR = os.path.dirname(BASE_DIR)
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
 SECRETS_DIR = os.path.join(ROOT_DIR, '.secrets')
 secrets = json.load(open(os.path.join(SECRETS_DIR, 'base.json')))
 SECRET_KEY = secrets['SECRET_KEY']
