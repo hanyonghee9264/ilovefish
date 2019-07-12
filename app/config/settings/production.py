017375c6b8997451fb0f193960b8ebca73365de7
hanyonghee <hanyonghee9264@gmail.com>
2019-07-12 15:37:09 +0900

    [DELETE] ECS healthcheck 삭제

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -58,14 +58,6 @@ AWS_S3_REGION_NAME = 'ap-northeast-2'
 # s3
 AWS_DEFAULT_ACL = None
 
-
-# Health Check 도메인을 허용하는 코드 [ECS]
-try:
-    EC2_IP = requests.get('http://169.254.169.254/latest/meta-data/local-ipv4').text
-    ALLOWED_HOSTS.append(EC2_IP)
-except requests.exceptions.RequestException:
-    pass
-
 # 로그폴더 생성
 LOG_DIR = os.path.join(ROOT_DIR, '.log')
 # 로그 폴더가 존재하지 않으면 폴더 생성
