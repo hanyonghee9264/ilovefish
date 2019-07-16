bda3d8a0cec37dc62eaf51d4e2860d0dd71c9cc4
hanyonghee <hanyonghee9264@gmail.com>
2019-07-16 23:36:55 +0900

    [ADD] tool gitignore 추가

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 git add -f .secrets/
 git add -f .static/
+git add -f .tool/
 eb deploy --profile coffeecalorie-eb --staged
 git reset HEAD .secrets/
 git reset HEAD .static/
+git reset HEAD .tool/
