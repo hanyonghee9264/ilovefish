a67943f6d6be988f53670c85e5a61d6f2ef514f9
hanyonghee <hanyonghee9264@gmail.com>
2019-08-27 14:09:22 +0900

    [UPDATE] 투썸플레이스 전체보기 기능 추가

--- app/coffeeshop/urls.py
+++ app/coffeeshop/urls.py
@@ -7,4 +7,5 @@ app_name = 'coffeeshop'
 urlpatterns = [
     path('starbucks/', views.starbucks_total_list, name='starbucks'),
     path('starbucks/<str:category>/', views.starbucks_category_coffee, name='starbucks-category'),
+    path('twosomeplace/', views.twosome_total_list, name='twosome'),
 ]
--- app/coffeeshop/views.py
+++ app/coffeeshop/views.py
@@ -5,33 +5,32 @@ from .models import Coffee, CoffeeCategory, CoffeeImage
 
 
 def starbucks_total_list(request):
-    details = Coffee.objects.all()
+    starbucks = Coffee.objects.filter(coffeeshop_list='STARBUCKS')
 
     # pagination (전체보기 적용)
-    paginator = Paginator(details, 6)
-
+    paginator = Paginator(starbucks, 6)
     page = request.GET.get('page')
 
     try:
-        details = paginator.page(page)
+        starbucks = paginator.page(page)
     except PageNotAnInteger:
-        details = paginator.page(1)
+        starbucks = paginator.page(1)
     except EmptyPage:
-        details = paginator.page(paginator.num_pages)
+        starbucks = paginator.page(paginator.num_pages)
 
     # url의 쿼리스트링을 가져옴. 없는 경우 None을 리턴
-    calorie = request.GET.get('calorie')
+    calorie = request.GET.get('calorie', '')
 
     if calorie == 'high':
-        details = Coffee.objects.all().order_by('-calorie')
-        return render(request, 'coffeeshop/starbucks_total_list.html', {'details': details})
+        starbucks = Coffee.objects.filter(coffeeshop_list='STARBUCKS').order_by('-calorie')
+        return render(request, 'coffeeshop/starbucks_total_list.html', {'starbucks': starbucks})
 
     elif calorie == 'low':
-        details = Coffee.objects.all().order_by('calorie')
-        return render(request, 'coffeeshop/starbucks_total_list.html', {'details': details})
+        starbucks = Coffee.objects.filter(coffeeshop_list='STARBUCKS').order_by('calorie')
+        return render(request, 'coffeeshop/starbucks_total_list.html', {'starbucks': starbucks})
 
     context = {
-        'details': details,
+        'starbucks': starbucks,
     }
     return render(request, 'coffeeshop/starbucks_total_list.html', context)
 
@@ -53,3 +52,36 @@ def starbucks_category_coffee(request, category):
         'coffee': coffee,
     }
     return render(request, 'coffeeshop/starbucks_category_coffee.html', context)
+
+
+# 투썸 커피 전체보기
+
+def twosome_total_list(request):
+    twosome = Coffee.objects.filter(coffeeshop_list='ATWOSOMEPLACE')
+
+    # pagination (전체보기 적용)
+    paginator = Paginator(twosome, 6)
+    page = request.GET.get('page')
+
+    try:
+        twosome = paginator.page(page)
+    except PageNotAnInteger:
+        twosome = paginator.page(1)
+    except EmptyPage:
+        twosome = paginator.page(paginator.num_pages)
+
+    # url의 쿼리스트링을 가져옴. 없는 경우 None을 리턴
+    calorie = request.GET.get('calorie', '')
+
+    if calorie == 'high':
+        twosome = Coffee.objects.filter(coffeeshop_list='ATWOSOMEPLACE').order_by('-calorie')
+        return render(request, 'coffeeshop/twosome_total_list.html', {'twosome': twosome})
+
+    elif calorie == 'low':
+        twosome = Coffee.objects.filter(coffeeshop_list='ATWOSOMEPLACE').order_by('calorie')
+        return render(request, 'coffeeshop/twosome_total_list.html', {'twosome': twosome})
+
+    context = {
+        'twosome': twosome,
+    }
+    return render(request, 'coffeeshop/twosome_total_list.html', context)
