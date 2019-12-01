591fc43df9ad3a393f827134465e44a206eff41f
hanyonghee <hanyonghee9264@gmail.com>
2019-12-02 01:05:31 +0900

    [UPDATE] 이전 coffeecalorie 템플릿으로 다시 복구 #1

--- app/config/views.py
+++ app/config/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def main_page(request):
-    return render(request, 'coffeeshop/homepage_main.html')
+    return render(request, 'coffeeshop/coffeecalorie_main_page.html')
